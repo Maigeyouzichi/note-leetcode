457ff5b50454899d48940cf8573bd74737401a0f
lihao <maigeyouzichi@gmail.com>
2022-12-26 13:32:58 +0800

    2,两数相加

---
+++ medium/src/main/java/com/leetcode/medium/Solution1.java
@@ -0,0 +1,33 @@
+package com.leetcode.medium;
+
+import com.leetcode.base.ListNode;
+
+/**
+ * 计划前50道题目为一个类文件
+ * @author lihao on 2022/12/26
+ */
+public class Solution1 {
+
+    /**
+     * 2,两数相加 https://leetcode.cn/problems/add-two-numbers/
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode curr = head;
+        //保存进位数
+        int k = 0;
+        while(l1 != null || l2 != null) {
+            int first = l1 == null ? 0 : l1.val;
+            int second = l2 == null ? 0 : l2.val;
+            int val = first + second + k;
+            curr.next = new ListNode(val%10);
+            curr = curr.next;
+            k = val/10;
+            l1 = l1==null?null:l1.next;
+            l2 = l2==null?null:l2.next;
+        }
+        if(k>0) curr.next = new ListNode(k);
+        return head.next;
+    }
+
+}
