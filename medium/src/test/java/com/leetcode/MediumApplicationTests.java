cbc63454d4a88cb61044648965045bf7d38e5b8f
lihao <maigeyouzichi@gmail.com>
2022-12-26 13:07:40 +0800

    init project

---
+++ medium/src/test/java/com/leetcode/MediumApplicationTests.java
@@ -0,0 +1,14 @@
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("hello medium");
+	}
+
+}
