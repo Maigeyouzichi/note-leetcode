d6ff10c03aa30108fe135a229006834d63bbb204
lihao <maigeyouzichi@gmail.com>
2023-01-14 17:53:28 +0800

    63. 不同路径 II

--- medium/src/main/java/com/leetcode/medium/Solution.java
+++ medium/src/main/java/com/leetcode/medium/Solution.java
@@ -601,5 +601,39 @@ public class Solution {
         return dp[m-1][n-1];
     }
 
+    /**
+     * 63. 不同路径 II https://leetcode.cn/problems/unique-paths-ii/
+     * 思路: 动态规划
+     * do[i][j]表示机器人走到(从0开始)为i,j的位置,一共有多少不同的走法.有障碍物的格子,dp[i][j]=0
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                break;
+            } else {
+                dp[i][0] = 1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                break;
+            } else {
+                dp[0][i] = 1;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
 
 }
