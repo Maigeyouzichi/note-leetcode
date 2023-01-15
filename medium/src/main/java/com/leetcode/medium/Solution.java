1937496c069e1ebaf7a4d10e46a5016b88464bd2
lihao <maigeyouzichi@gmail.com>
2023-01-15 12:21:05 +0800

    64. 最小路径和

--- medium/src/main/java/com/leetcode/medium/Solution.java
+++ medium/src/main/java/com/leetcode/medium/Solution.java
@@ -635,5 +635,27 @@ public class Solution {
         return dp[m - 1][n - 1];
     }
 
+    /**
+     * 64. 最小路径和 https://leetcode.cn/problems/minimum-path-sum/
+     * 思路: 动态规划
+     * dp[i][j]含义是到达位置(i,j)的最小路径和
+     */
+    public int minPathSum(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        for(int i=1;i<grid.length;i++) {
+            dp[i][0] = dp[i-1][0]+grid[i][0];
+        }
+        for(int i=1;i<grid[0].length;i++) {
+            dp[0][i] = dp[0][i-1]+grid[0][i];
+        }
+        for(int i=1;i<grid.length;i++) {
+            for(int j=1;j<grid[0].length;j++) {
+                dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1]) + grid[i][j];
+            }
+        }
+        return dp[grid.length-1][grid[0].length-1];
+    }
+
 
 }
