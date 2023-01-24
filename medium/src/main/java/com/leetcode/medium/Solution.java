f5ce5f52533762319dba1108f8092174d9ee19c2
lihao <maigeyouzichi@gmail.com>
2023-01-24 23:49:12 +0800

    198. 打家劫舍

--- medium/src/main/java/com/leetcode/medium/Solution.java
+++ medium/src/main/java/com/leetcode/medium/Solution.java
@@ -1016,5 +1016,22 @@ public class Solution {
         }
     }
 
+    /**
+     * 198. 打家劫舍 https://leetcode.cn/problems/house-robber/
+     * 思路: 动态规划
+     * dp[i]表示前i+1个房子可以偷到的财产最大总和,前两家单独讨论.
+     */
+    public int rob(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        if(nums.length == 2) return Math.max(nums[0],nums[1]);
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++) {
+            dp[i] = Math.max(dp[i-2]+nums[i],dp[i-1]);
+        }
+        return dp[nums.length-1];
+    }
+
 
 }
