e57c8200127d0520d7d5cfde6ce65eee10127d4d
lihao <maigeyouzichi@gmail.com>
2023-02-13 19:02:33 +0800

    面试题 01.08. 零矩阵

--- medium/src/main/java/com/leetcode/medium/Solution.java
+++ medium/src/main/java/com/leetcode/medium/Solution.java
@@ -1948,4 +1948,26 @@ public class Solution {
         }
         return true;
     }
+
+    /**
+     * 面试题 01.08. 零矩阵 https://leetcode.cn/problems/zero-matrix-lcci/
+     * 思路: 遍历
+     */
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> lineSet = new HashSet<>();
+        Set<Integer> columnSet = new HashSet<>();
+        for(int i =0;i<matrix.length;i++) {
+            for(int j=0;j<matrix[0].length;j++) {
+                if(matrix[i][j] == 0) {
+                    lineSet.add(i);
+                    columnSet.add(j);
+                }
+            }
+        }
+        for(int i =0;i<matrix.length;i++) {
+            for(int j=0;j<matrix[0].length;j++) {
+                if(lineSet.contains(i) || columnSet.contains(j)) matrix[i][j] = 0;
+            }
+        }
+    }
 }
